ef19466505a5794aadc866003fe232a364e478c3
imzyao <13593554644@163.com>
2024-12-21 20:48:22 +0800

    优化

--- api-01/src/main/java/cn/mzyao/cloud/Api01Application.java
+++ api-01/src/main/java/cn/mzyao/cloud/Api01Application.java
@@ -3,7 +3,9 @@ package cn.mzyao.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class Api01Application {
 
--- commons/src/main/java/cn/mzyao/cloud/tools/filter/GatewayFilter.java
+++ commons/src/main/java/cn/mzyao/cloud/tools/filter/GatewayFilter.java
@@ -1,6 +1,7 @@
 package cn.mzyao.cloud.tools.filter;
 
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpStatus;
 import cn.hutool.json.JSONUtil;
 import cn.mzyao.cloud.tools.enums.ResponseCode;
 import cn.mzyao.cloud.tools.result.Result;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -27,14 +29,15 @@ public class GatewayFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         String gateway = request.getHeader("gatewayKey");
         if (StrUtil.isEmpty(gateway) || !gateway.equals("key")) {
             log.error("非法请求");
-            servletResponse.setContentType("text/json;charset=UTF-8");
-            servletResponse.getWriter().write(JSONUtil.toJsonStr(Result.fail(ResponseCode.ILLEGALITY_ERROR)));
+            response.setContentType("text/json;charset=UTF-8");
+            response.getWriter().write(JSONUtil.toJsonStr(Result.fail(ResponseCode.ILLEGALITY_ERROR)));
             return;
         }
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
     }
 }
