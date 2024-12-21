d836a7c47a81e21c85b556e05f04e3e4ab1efe9a
imzyao <13593554644@163.com>
2024-12-21 16:37:39 +0800

    初步接入网关gateway

---
+++ gateway/src/main/java/cn/mzyao/cloud/GatewayApplication.java
@@ -0,0 +1,12 @@
+package cn.mzyao.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ gateway/src/main/java/cn/mzyao/cloud/controller/WebController.java
@@ -0,0 +1,19 @@
+package cn.mzyao.cloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    /**
+     * 测试
+     *
+     * @return
+     */
+    @GetMapping("index")
+    public String index() {
+        return "Hello,Gateway";
+    }
+
+}
