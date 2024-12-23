ae7dbf40b219b895b61d1b13fbf3be68713664f1
imzyao <13593554644@163.com>
2024-12-23 15:32:18 +0800

    优化

--- api-01/src/main/java/cn/mzyao/cloud/Api01Application.java
+++ api-01/src/main/java/cn/mzyao/cloud/Api01Application.java
@@ -1,10 +1,11 @@
 package cn.mzyao.cloud;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @ServletComponentScan
 @SpringBootApplication
 public class Api01Application {
